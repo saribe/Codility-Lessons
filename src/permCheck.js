3a5622f727cf6b36dfa76899121c6de9472d857e
Samuel Pinto <samuel@maispc.com>
2016-01-07 20:59:09 +0000

    Permutation check
    Use a bit array to track elements

---
+++ src/permCheck.js
@@ -0,0 +1,12 @@
+﻿function permCheck(array) {
+    var i = 0;
+    var l = array.length;
+    var bit = [];
+
+    for (; i < l; bit[array[i++]] = true);
+    for (i = 1; i <= l; i++) {
+        if (!bit[i]) return 0;
+    }
+
+    return 1;
+}
