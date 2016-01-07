69edd642c8c61445fde20164263088deabdbfda2
Samuel Pinto <samuel@maispc.com>
2016-01-07 20:56:42 +0000

    Frog River one lesson
    Map all needed leafs and count them to destination reach destination.

---
+++ src/frogRiverOne.js
@@ -0,0 +1,17 @@
+﻿function frogRiverOne(position, array) {
+    var route = {};
+    var steps = position;
+    var value;
+
+    for (var i = 0; i < array.length; i++) {
+        value = array[i];
+
+        if (!route[value] && value <= position) {
+            route[value] = true;
+
+            if (!--steps) return i;
+        }
+    }
+
+    return -1;
+}
