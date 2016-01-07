68689b3de18c868d4b309df4d7d55b8127883862
Samuel Pinto <samuel@maispc.com>
2016-01-07 21:00:22 +0000

    Tape equilibrium lesson

---
+++ src/tapeEquilibrium.js
@@ -0,0 +1,24 @@
+﻿/**
+ * Absolute difference between the sum of the
+ * first part and the sum of the second part.
+ * @param {Array} array
+ * @returns {Number}
+ */
+function tapeEquilibrium(array) {
+    var i = 0;
+    var l = array.length;
+    var lSum = 0;
+    var min = ~(1 << 31);
+    var rSum, diff;
+    var total = 0;
+
+    for (; i < l; total += array[i++]);
+    for (i = 0; i < l - 1; i++) {
+        lSum += array[i];
+        rSum = total - lSum;
+        diff = Math.abs(lSum - rSum);
+        if (diff < min) min = diff;
+    }
+
+    return min;
+}
