5d83c3d881bebac33b252b3562ac2025bef9c0eb
Samuel Pinto <samuel@maispc.com>
2016-01-07 20:58:44 +0000

    Perm missing elements
    Assuming that there is no repeated elements, we can get the expected
    value if all values were available.
    Then subtract all existing values, the remain will be the missing
    element!

---
+++ src/permMissingElem.js
@@ -0,0 +1,15 @@
+﻿/**
+ * A zero-indexed array A consisting of N different integers is given.
+ * The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+ * Your goal is to find that missing element.
+ * @param {Array} array
+ * @returns {Number}
+ */
+function permMissingElem(array) {
+    var n = array.length + 1;               // number of consecutive numbers
+    var expected = (n * (n + 1)) / 2;       // get sum of consecutive numbers
+
+    array.forEach(v => expected -= v);      // subtract all existing numbers
+
+    return expected;                        // the missing number
+}
