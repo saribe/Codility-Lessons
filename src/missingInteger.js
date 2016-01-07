2393f67802af5838aec17fd4c717f8a5292ffce4
Samuel Pinto <samuel@maispc.com>
2016-01-07 21:00:05 +0000

    Missing Integer lesson
    Use a bit array to track non repeated existing values, and then check
    what is the first missing.

---
+++ src/missingInteger.js
@@ -0,0 +1,21 @@
+﻿/**
+ * Find the minimal positive integer not occurring in a given sequence.
+ * @param {Array} non-empty zero-indexed array of integers
+ * @returns {Number} minimal positive integer (greater than 0) that does not occur in array
+ */
+function missingInteger(array) {
+    var orderArrayWithoutRepeated = [];
+    var i;
+
+    for (i = 0; i < array.length; i++) {
+        if (array[i] > 0) {
+            orderArrayWithoutRepeated[array[i]] = array[i];
+        }
+    }
+
+    for (i = 1; i <= orderArrayWithoutRepeated.length; i++) {
+        if (!orderArrayWithoutRepeated[i]) return i;
+    }
+
+    return 1;
+}
